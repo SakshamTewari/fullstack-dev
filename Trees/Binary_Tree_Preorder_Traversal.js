34ef8554b361b91a264590a45553201a75956977
Saksham Tewari <sakshamtewari3@gmail.com>
2024-08-10 21:48:14 +0530

    Binary Tree Preorder Traversal

---
+++ Trees/Binary_Tree_Preorder_Traversal.js
@@ -0,0 +1,35 @@
+/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  let res = [];
+
+  function preorder(root) {
+    if (root === null) {
+      return;
+    }
+    res.push(root.val);
+    preorder(root.left);
+    preorder(root.right);
+  }
+
+  preorder(root);
+  return res;
+};
