3f55d469a1a0bb1a0accb78e7f9f7ffef3f19d29
Saksham Tewari <sakshamtewari3@gmail.com>
2024-08-18 00:56:43 +0530

    Count Odd Numbers In An Interval Range

---
+++ Math & Geometry/Count_Odd_Numbers_In_An_Interval_Range.js
@@ -0,0 +1,24 @@
+/*
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+*/
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+
+var countOdds = function (low, high) {
+  let count = 0;
+  while (low <= high) {
+    if (low % 2 !== 0) count++;
+    low++;
+  }
+
+  return count;
+};
