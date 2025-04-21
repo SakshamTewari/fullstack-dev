87133a5663ff6a3cfd9b5f5554caeafd2d943d2c
Saksham Tewari <sakshamtewari3@gmail.com>
2025-04-21 21:16:47 +0530

    abort an API request

---
+++ JavaScript/abort_API.js
@@ -0,0 +1,53 @@
+const controller = new AbortController();
+const signal = controller.signal;
+
+const downloadBtn = document.querySelector('.download');
+const abortBtn = document.querySelector('.abort');
+
+// helper method to make api call
+function makeCall() {
+  fetch('https://jsonplaceholder.typicode.com/todos/1', { signal })
+    .then((response) => {
+      console.log('complete', response);
+    })
+    .catch((err) => {
+      console.log(`error: ${err.message}`);
+    });
+}
+
+//download event
+downloadBtn.addEventListener('click', makeCall);
+
+//abort event
+abortBtn.addEventListener('click', () => {
+  controller.abort(); // this aborts the controller signal permanently and new fetch request will throw an error
+  console.log('Download aborted');
+});
+
+/* To fix above issue, we should create new controller + signal for every new download
+
+let controller; // define outside
+
+const downloadBtn = document.querySelector('.download');
+const abortBtn = document.querySelector('.abort');
+
+downloadBtn.addEventListener('click', () => {
+  controller = new AbortController(); // new one every time
+  const signal = controller.signal;
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1', { signal })
+    .then((response) => {
+      console.log('complete', response);
+    })
+    .catch((err) => {
+      console.log(`error: ${err.message}`);
+    });
+});
+
+abortBtn.addEventListener('click', () => {
+  if (controller) {
+    controller.abort();
+    console.log('Download aborted');
+  }
+});
+*/
