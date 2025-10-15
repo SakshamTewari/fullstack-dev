e4d3a5ada1271c7dab5671a2133bb30f610880d9
Saksham Tewari <sakshamtewari3@gmail.com>
2025-10-15 21:24:46 +0530

    add: Longest Common Subsequence (recursion)

---
+++ Dynamic Programming/Longest_Common_Subsequence.js
@@ -0,0 +1,43 @@
+/*
+Given two strings, s1 and s2, the task is to find the length of the Longest Common Subsequence. 
+If there is no common subsequence, return 0. A subsequence is a string generated from the original string by deleting 0 or more characters, without changing the relative order of the remaining characters.
+
+For example, subsequences of "ABC" are "", "A", "B", "C", "AB", "AC", "BC" and "ABC". In general, a string of length n has 2n subsequences.
+
+Examples:
+
+Input: s1 = "ABC", s2 = "ACD"
+Output: 2
+Explanation: The longest subsequence which is present in both strings is "AC".
+
+Input: s1 = "AGGTAB", s2 = "GXTXAYB"
+Output: 4
+Explanation: The longest common subsequence is "GTAB".
+
+Input: s1 = "ABC", s2 = "CBA"
+Output: 1
+Explanation: There are three longest common subsequences of length 1, "A", "B" and "C".
+*/
+
+function longestCommonSubsequenceRecursion(
+  s1,
+  s2,
+  m = s1.length,
+  n = s2.length,
+) {
+  // base case
+  if (m === 0 || n === 0) return 0;
+
+  // if characters match
+  if (s1[m - 1] === s2[n - 1]) {
+    return 1 + longestCommonSubsequenceRecursion(s1, s2, m - 1, n - 1);
+  }
+  // if characters don't match
+  return Math.max(
+    longestCommonSubsequenceRecursion(s1, s2, m, n - 1),
+    longestCommonSubsequenceRecursion(s1, s2, m - 1, n),
+  );
+}
+
+// Test
+console.log(longestCommonSubsequenceRecursion('AGGTAB', 'GXTXAYB'));
