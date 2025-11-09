fb05775649142575a81c9e1e5d4c0e1a67416c93
Saksham Tewari <sakshamtewari3@gmail.com>
2025-11-09 23:01:07 +0530

    update: brute force to check duplicates within K

--- Sliding Window/Contains_Duplicates_II.js
+++ Sliding Window/Contains_Duplicates_II.js
@@ -12,6 +12,8 @@ Output: true
  * @param {number} k
  * @return {boolean}
  */
+
+// Using Hashset  [O(n) time, O(k) space]
 var containsNearbyDuplicate = function (nums, k) {
   let set = new Set();
   for (let i = 0; i < nums.length; i++) {
@@ -22,3 +24,17 @@ var containsNearbyDuplicate = function (nums, k) {
 
   return false;
 };
+
+// Using Brute force [O(n*k) time, O(1) space]
+function containsNearbyDuplicateBruteForce(nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j <= k && i + j < nums.length; j++) {
+      if (nums[i] === nums[i + j]) return true;
+    }
+  }
+  return false;
+}
+
+// Test
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
+console.log(containsNearbyDuplicateBruteForce([1, 2, 3, 1], 3));
