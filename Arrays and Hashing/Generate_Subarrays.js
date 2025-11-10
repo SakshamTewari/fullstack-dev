161808fdc5293f50f61b750448d832e10e2308e5
Saksham Tewari <sakshamtewari3@gmail.com>
2025-11-10 20:52:41 +0530

    update: Sum of all subarrays added

--- Arrays and Hashing/Generate_Subarrays.js
+++ Arrays and Hashing/Generate_Subarrays.js
@@ -41,6 +41,38 @@ function generateSubarraysRecursive(arr, result = [], i = 0, j = 0) {
   return generateSubarraysRecursive(arr, result, i, j + 1);
 }
 
+/*
+Sum of all Subarrays
+
+Given an integer array arr[], compute the sum of all possible sub-arrays of the array. A sub-array is a contiguous part of the array.
+
+Examples: 
+
+Input: arr[] = [1, 4, 5, 3, 2]
+Output: 116
+Explanation: Sum of all possible subarrays of the array [1, 4, 5, 3, 2] is 116.
+
+Input: arr[] = [1, 2, 3, 4]
+Output: 50
+Explanation: Sum of all possible subarrays of the array [1, 2, 3, 4] is 50.
+*/
+
+function sumOfAllSubarrays(arr) {
+  let total = 0;
+
+  let i = 0;
+  while (i < arr.length) {
+    let subarraySum = 0;
+    for (let j = i; j < arr.length; j++) {
+      subarraySum += arr[j];
+      total += subarraySum;
+    }
+    i++;
+  }
+  return total;
+}
+
 // Test
 console.log(generateSubarrays([1, 2, 3]));
 console.log(generateSubarraysRecursive([1, 2, 3]));
+console.log(sumOfAllSubarrays([1, 4, 5, 3, 2]));
