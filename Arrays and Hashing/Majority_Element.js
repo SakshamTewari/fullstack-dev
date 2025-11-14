d647fcc6c4f64a1417715a7cb70a3c0ef40aeafa
Saksham Tewari <sakshamtewari3@gmail.com>
2025-11-14 21:32:58 +0530

    add: Moore's Voting Algorithm

--- Arrays and Hashing/Majority_Element.js
+++ Arrays and Hashing/Majority_Element.js
@@ -11,6 +11,8 @@ Output: 3
  * @param {number[]} nums
  * @return {number}
  */
+
+// Using HashMap
 var majorityElement = function (nums) {
   let frequencyMap = new Map();
   for (let num of nums) {
@@ -27,3 +29,56 @@ var majorityElement = function (nums) {
   }
   return 0;
 };
+
+// Using Sorting
+function majorityElementSorting(nums) {
+  nums.sort((a, b) => a - b);
+
+  let candidate = nums[Math.floor(nums.length / 2)];
+
+  let count = 0;
+  for (let num of nums) {
+    if (num === candidate) {
+      count++;
+    }
+  }
+
+  if (count > Math.floor(nums.length / 2)) return candidate;
+  else return -1;
+}
+
+// Using Moore's Voting Algorithm  [O(n) time, O(1) space]
+/*
+Imagine you are voting:
+
+Every time you see the same number, you increase its count.
+When you see a different number, you cancel out one vote.
+Majority element will survive all cancellations because it appears more than others.
+
+You end with 1 candidate, which must be the majority.
+ */
+function majorityElementMoore(nums) {
+  let candidate = null;
+  let count = 0;
+
+  for (let num of nums) {
+    if (count === 0) {
+      candidate = num;
+    }
+    count += num === candidate ? 1 : -1;
+  }
+
+  // verify candidate: only required when majority element is not gauranteed to exist
+  // else just return candidate
+  let freq = 0;
+  for (let num of nums) {
+    if (num === candidate) freq++;
+  }
+
+  return freq > nums.length / 2 ? candidate : -1;
+}
+
+// Test
+console.log(majorityElement([1, 1, 2, 1, 3, 5, 1]));
+console.log(majorityElementSorting([1, 1, 2, 1, 3, 5, 1]));
+console.log(majorityElementMoore([1, 1, 2, 1, 3, 5, 1]));
