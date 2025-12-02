d4d57df5e176cf0eeda2394546d42119f20458f4
Saksham Tewari <sakshamtewari3@gmail.com>
2025-12-02 15:17:00 +0530

    update: throttle an array of tasks

--- JavaScript/throttle.js
+++ JavaScript/throttle.js
@@ -5,6 +5,7 @@ If the user keeps on typing then reset the function.
 
 Throttling:- 
 It is used to restrict the no of times a function can be called/invoked/executed. 
+Throttling is a way/technique to restrict the number of function execution/calls.
 For example, making an API call to the server on the user’s click. 
 If the user spam the click then also there will be specific calls only. Like, make each call after 10 seconds. */
 
@@ -13,18 +14,57 @@ const throttle = (func, limit) => {
   let lastRan;
 
   return function () {
-    const args = arguments;
+    const args = arguments; // Store arguments
+    const context = this; // Store `this` for later use
+
     if (!lastRan) {
-      func.apply(this, args);
+      func.apply(context, args);
       lastRan = Date.now();
     } else {
       clearTimeout(lastFunc);
       lastFunc = setTimeout(() => {
         if (Date.now() - lastRan >= limit) {
-          func.apply(this, args);
+          func.apply(context, args); // Use stored args & context
           lastRan = Date.now();
         }
       }, limit - (Date.now() - lastRan));
+    }
+  };
+};
+
+/*
+Implement a throttler that executes an array of tasks. 
+When the  throttler is passed a number, only executes that number of the tasks and  passes the other tasks into a queue
+*/
+
+const throttleTasks = (tasks, count = tasks.length, callback, delay = 1000) => {
+  let lastFunc;
+  let lastRan;
+
+  // track the task
+  let queue = [];
+
+  return function () {
+    // store context
+    const context = this;
+    const args = arguments;
+
+    //if the throttle is exectured the first time, run it immediately
+    if (!lastRan) {
+      queue = [...queue, ...tasks];
+      const execute = queue.splice(0, count);
+      callback(execute);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(() => {
+        if (Date.now() - lastRan >= delay) {
+          queue = [...queue, ...tasks];
+          const execute = queue.splice(0, count);
+          callback(execute);
+          lastRan = Date.now();
+        }
+      }, delay - (Date.now() - lastRan));
     }
   };
 };
