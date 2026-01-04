c9f1848d8e513fe80e4c3522b99113fe9df879d9
Saksham Tewari <sakshamtewari3@gmail.com>
2026-01-04 16:14:08 +0530

    add: Swap nodes pairwise (iterative)

---
+++ Linked List/Swap_Nodes.js
@@ -0,0 +1,44 @@
+/*
+Given a singly linked list, the task is to swap linked list elements pairwise.
+
+Examples:
+
+Input : 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL 
+Output : 2 -> 1 -> 4 -> 3 -> 6 -> 5 -> NULL
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+function swapNodes(head) {
+  let curr = head;
+
+  while (curr !== null && curr.next !== null) {
+    [curr.data, curr.next.data] = [curr.next.data, curr.data];
+    curr = curr.next.next;
+  }
+}
+
+function printList(head) {
+  let curr = head;
+  while (curr !== null) {
+    console.log(curr.data);
+    curr = curr.next;
+  }
+}
+
+// Test
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+
+swapNodes(head);
+
+printList(head);
