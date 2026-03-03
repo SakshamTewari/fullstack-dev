22fe72a64f57c834391e1dc75e161c6ea8835e96
Saksham Tewari <sakshamtewari3@gmail.com>
2026-03-03 15:07:32 +0530

    update: Check power of 2 using bit manipulation

--- Bit Manipulation/Power_Of_Two.js
+++ Bit Manipulation/Power_Of_Two.js
@@ -1,15 +1,13 @@
 /*
 Given an integer n, return true if it is a power of two. Otherwise, return false.
-
 An integer n is a power of two, if there exists an integer x such that n == 2x.
 
  
-
 Example 1:
 
 Input: n = 1
 Output: true
-Explanation: 20 = 1
+Explanation: 2^0 = 1
 */
 /**
  * @param {number} n
@@ -30,3 +28,12 @@ var isPowerOfTwo = function (n) {
   }
   return false;
 };
+
+// Using Bit Mainpulation    8 = 1000 , 7 = 0111  8 & 7 = 0
+function isPowerOfTwoBit(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+}
+
+// Test
+console.log(isPowerOfTwoBit(8));
+console.log(isPowerOfTwoBit(7));
