568ce3dedb608b893be9f1000b1808b5af203e1e
Saksham Tewari <sakshamtewari3@gmail.com>
2026-03-07 22:08:35 +0530

    update: Max Consecutive Ones after flipping 0s

--- Arrays and Hashing/Max_Consecutive_Ones.js
+++ Arrays and Hashing/Max_Consecutive_Ones.js
@@ -1,9 +1,6 @@
 /*
 Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
- 
-Example 1:
-
 Input: nums = [1,1,0,1,1,1]
 Output: 3
 Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
@@ -27,3 +24,35 @@ var findMaxConsecutiveOnes = function (nums) {
   }
   return maxCount;
 };
+
+/*
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Input : nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0] , k = 3
+Output : 10
+Explanation : The maximum number of consecutive 1's are obtained only if we flip the 0's present at position 3, 4, 5 (0 base indexing).
+The array after flipping becomes [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0].
+The number of consecutive 1's is 10.
+*/
+
+function longestOnes(nums, k) {
+  let left = 0;
+  let zeroCount = 0;
+  let maxLen = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    if (nums[right] === 0) zeroCount++;
+
+    while (zeroCount > k) {
+      if (nums[left] === 0) zeroCount--;
+      left++;
+    }
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+  return maxLen;
+}
+
+// Test
+
+console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]));
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 3));
