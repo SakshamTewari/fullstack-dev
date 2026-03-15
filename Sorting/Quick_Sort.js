aa04be4e74bc4afbc154027b6b2cd8febbaca949
Saksham Tewari <sakshamtewari3@gmail.com>
2026-03-15 22:07:12 +0530

    add: Quick Sort

---
+++ Sorting/Quick_Sort.js
@@ -0,0 +1,33 @@
+/*
+Pick a pivot and partition the array so smaller elements go left and larger go right.
+Recursively quicksort the left and right subarrays.
+
+Divide and Conquer
+*/
+
+function quickSort(arr, low = 0, high = arr.length - 1) {
+  if (low < high) {
+    const pivotIndex = partition(arr, low, high);
+    quickSort(arr, low, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, high);
+  }
+  return arr;
+}
+
+function partition(arr, low, high) {
+  let pivot = arr[high];
+  let i = low - 1;
+
+  for (let j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      // swap
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;
+}
+
+// Test
+console.log(quickSort([3, 2, 5, 1, 6]));
